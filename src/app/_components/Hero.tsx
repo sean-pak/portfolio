dda3f748a26b6853109f7c7442cd1f3ab23aa28f
Sean Pak <spak16@jhu.edu>
2024-02-01 16:50:11 -0500

    Initial Commit

---
+++ src/app/_components/Hero.tsx
@@ -0,0 +1,51 @@
+"use client";
+import React, { useState } from "react";
+
+function Hero() {
+  const [showFirstVideo, setShowFirstVideo] = useState(true);
+  const [showSecondVideo, setShowSecondVideo] = useState(false);
+
+  const playSecondVideo = () => {
+    const secondVideo = document.getElementById("name-animation-end");
+    if (secondVideo) {
+      setShowFirstVideo(false);
+      setShowSecondVideo(true);
+      (secondVideo as HTMLVideoElement).play();
+    }
+  };
+
+  return (
+    <div>
+      <video
+        id="name-animation"
+        width="100%"
+        height="100%"
+        autoPlay
+        muted
+        playsInline
+        onEnded={playSecondVideo}
+        style={{ display: showFirstVideo ? "block" : "none" }}
+      >
+        <source
+          src="https://res.cloudinary.com/daxlv1ing/video/upload/v1706818312/name-animation.mp4"
+          type="video/mp4"
+        />
+        Your browser does not support the video tag.
+      </video>
+      <video
+        id="name-animation-end"
+        width="100%"
+        autoPlay
+        loop
+        style={{ display: showSecondVideo ? "block" : "none" }}
+      >
+        <source
+          src="https://res.cloudinary.com/daxlv1ing/video/upload/v1706823739/long-name-animation-end.mp4"
+          type="video/mp4"
+        />
+      </video>
+    </div>
+  );
+}
+
+export default Hero;
