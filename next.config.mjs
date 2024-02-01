7c06149548f11adcaf54b2a44fbcaa4e62bbbf5e
Sean Pak <spak16@jhu.edu>
2024-02-01 17:16:42 -0500

    New next.config.js file

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-import { withNextVideo } from "next-video/process";
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-};
-
-module.exports = nextConfig;
-
-export default withNextVideo(nextConfig);
