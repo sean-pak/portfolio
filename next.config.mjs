e576dcdbee1cd0254c4366694c2af897d60b8b46
Sean Pak <spak16@jhu.edu>
2024-02-01 17:13:36 -0500

    Resolved obsolete next export

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 import { withNextVideo } from "next-video/process";
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+};
+
+module.exports = nextConfig;
 
 export default withNextVideo(nextConfig);
